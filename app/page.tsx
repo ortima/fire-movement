52d4a3784414f3b294996e66350bec015568e288
ortima <account.net@mail.ru>
2024-08-20 00:06:50 +0300

    ✨ (feat): Added login and registration page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { CalculatorT, Categories } from "@/app/types/calculator";
-import { CategoryCard } from "@/components/shared/card/category-card";
+import { CategoryCard } from "@/components/shared/card/CategoryCard";
 import { useAppSelector } from "@/lib/redux/hooks";
+import { useSession } from "next-auth/react";
 
-const Home = () => {
+const CalculatorPage = () => {
   const calculator = useAppSelector((state) => state.calculator);
-
+  const session = useSession();
+  console.log("session", session);
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       {(Object.keys(Categories) as Array<keyof CalculatorT>).map((category) => (
@@ -20,4 +22,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default CalculatorPage;
